6fac1e705185f034f89655371a659f79b29a1f6b
jack <jack_ryan07@yahoo.com>
2021-04-06 22:39:03 -0500

    set up
    Project1

--- SimpliLearnProject1/src/main/java/com/example/SimpliLearnProject1/SimpliLearnProject1Application.java
+++ SimpliLearnProject1/src/main/java/com/example/SimpliLearnProject1/SimpliLearnProject1Application.java
@@ -1,18 +1,13 @@
-package com.example.SpringStarter;
+package com.example.SimpliLearnProject1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringStarterApplication {
+public class SimpliLearnProject1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringStarterApplication.class, args);
+		SpringApplication.run(SimpliLearnProject1Application.class, args);
 	}
 
-	
-	
-	
-	
-	
 }
